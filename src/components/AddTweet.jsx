179f53bb0c9b35c0a8baf9a1915c96b50b335db3
Harshit Shukla <mrharshit2409@gmail.com>
2024-02-06 18:34:08 +0530

    added edit btn and when onClick ---> input box

--- src/components/AddTweet.jsx
+++ src/components/AddTweet.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
+import './css/addTweet.css'
 
 function AddTweet({onAddTweet}) {
     const [text, setText] = useState("");
     return (
         <div className="add-tweet">
-            <input 
+            <input className="input-bar"
                 type="text" 
                 placeholder="Add new tweet..." 
                 value={text}
                 onChange={(event) => setText(event.target.value)}
             />
-            <button onClick={() => {
+            <button className="add-btn" onClick={() => {
                 onAddTweet(text)
                 setText('') //after clicking button every thing is cleared out from the input bar
             }}>
--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import './css/tweet.css'
 
 function Tweet({content, likeCount, createdAt}) {
+    const [isEditting, setIsEditting] = useState(false);
     return (
         <div className='tweet-wrapper'>
-            <div className="tweet-content">
-                {content}
+            
+            <div className='tweet-edit-wrapper'>
+                <div className="tweet-content">
+                    {(isEditting) ? <input /> : content}
+                </div>
+                <div className="edit-btn" onClick={() => setIsEditting(!isEditting)}>
+                    <button>{(isEditting) ? 'Save' : 'Edit'}</button>
+                </div>
             </div>
+
+            
             <div className="like-createdAt-wrapper">
                 <div className="likes">
                     {likeCount} likes
